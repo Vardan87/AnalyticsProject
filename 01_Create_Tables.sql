a101860e633349b5e63e34d9cb9e3a634606a3a8
Vardan Shahinyan <vardan87@gmail.com>
2026-05-19 21:39:51 +0400

    Add Department column to EmployeeSales

--- 01_Create_Tables.sql
+++ 01_Create_Tables.sql
@@ -1,7 +1,8 @@
--- 01_Create_Tables.sql
+-- 01_Create_Tables.sql (Modified)
 CREATE TABLE EmployeeSales (
 SaleID INT PRIMARY KEY IDENTITY(1,1),
 EmployeeName VARCHAR(100),
+Department VARCHAR(50), -- NEW COLUMN
 SaleAmount DECIMAL(10,2),
 TransactionDate DATE
 );
